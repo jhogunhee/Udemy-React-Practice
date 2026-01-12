62956358456720568f363d557ec75fec38a46508
jogunhee <rjsgml1677@naver.com>
2026-01-12 19:57:17 +0900

    조건적 콘텐츠 렌더링 연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,45 @@
+import {useState} from 'react'
 import {CORE_CONCEPTS} from '@/data.js';
 import {Header} from '@/components/Header/Header';
-
-const CoreConcept = ({image, title, description}) => {
-    return <li>
-        <img src={image} alt={title}/>
-        <h3>{title}</h3>
-        <p>{description}</p>
-    </li>
-}
+import CoreConcept from "@/components/CoreConcept/CoreConcept";
+import TabButton from "@/TabButton";
+import {EXAMPLES} from "@/example";
 
 function App() {
+    const [selectedTopic, setSelectedTopic] = useState(null);
+
+    const handleTabChange = (selectedTopic) => {
+        setSelectedTopic(selectedTopic);
+    }
     return (
         <div>
-            <Header />
+            <Header/>
             <main>
                 <section id="core-concepts">
                     <h2>Core Concepts</h2>
                     <ul>
-                        <CoreConcept {...CORE_CONCEPTS[0]} />
-                        <CoreConcept {...CORE_CONCEPTS[1]} />
-                        <CoreConcept {...CORE_CONCEPTS[2]} />
-                        <CoreConcept {...CORE_CONCEPTS[3]} />
+                        {CORE_CONCEPTS.map((item) => (
+                            <CoreConcept key={item.title} {... item} />
+                        ))};
                     </ul>
                 </section>
+                <section id="examples">
+                    <h2>Examples</h2>
+                    <menu>
+                        <TabButton isSelected={selectedTopic === 'components'} onSelect={() => handleTabChange('components')}>Components</TabButton>
+                        <TabButton isSelected={selectedTopic === 'jsx'} onSelect={() => handleTabChange('jsx')}>JSX</TabButton>
+                        <TabButton isSelected={selectedTopic === 'props'} onSelect={() => handleTabChange('props')}>Props</TabButton>
+                        <TabButton isSelected={selectedTopic === 'state'} onSelect={() => handleTabChange('state')}>State</TabButton>
+                    </menu>
+                </section>
+                    {!selectedTopic && <p>Please select a topic.</p>}
+                    {selectedTopic && (
+                        <div id="tab-content">
+                            <h3>{EXAMPLES[selectedTopic].title}</h3>
+                            <p>{EXAMPLES[selectedTopic].description}</p>
+                            <pre><code>{EXAMPLES[selectedTopic].code}</code></pre>
+                        </div>
+                    )}
             </main>
         </div>
     );
---
+++ src/TabButton.jsx
@@ -0,0 +1,7 @@
+const TabButton = ({children, onSelect, isSelected}) => {
+    return <li>
+        <button className={isSelected ? 'active' : undefined} onClick={onSelect}>{children}</button>
+    </li>
+}
+
+export default TabButton;
